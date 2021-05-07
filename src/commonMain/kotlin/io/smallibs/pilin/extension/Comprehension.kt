572342978a106a7cafb4f719e5bab95be10c7eaa
d-plaindoux <d.plaindoux@free.fr>
2021-05-07 10:46:20 +0200

    Update:Review comprehension mechanism separating monad from continuation

--- src/commonMain/kotlin/io/smallibs/pilin/extension/Comprehension.kt
+++ src/commonMain/kotlin/io/smallibs/pilin/extension/Comprehension.kt
@@ -6,28 +6,41 @@ import io.smallibs.pilin.type.App
 import kotlinx.coroutines.currentCoroutineContext
 import kotlin.coroutines.*
 
-class Comprehension<F, A>(private val m: Monad.Core<F>) : Monad.Core<F> by m, Continuation<App<F, A>> {
+class Comprehension<F, A>(private val m: Monad.Core<F>) : Monad.Core<F> by m {
 
-    private lateinit var ma: App<F, *>
+    private class ComprehensionContinuation<F, A> : Continuation<App<F, A>> {
+        // Cannot store a Result (inlined class)
+        private var ma: Any? = null
 
-    override val context: CoroutineContext
-        get() = EmptyCoroutineContext
+        override val context: CoroutineContext
+            get() = EmptyCoroutineContext
 
-    override fun resumeWith(result: Result<App<F, A>>) {
-        ma = result.getOrThrow() // Should not throw a n Exception
+        fun <B> intermediate(result: Result<App<F, B>>) {
+            ma = result
+        }
+
+        override fun resumeWith(result: Result<App<F, A>>) {
+            ma = result
+        }
+
+        @Suppress("UNCHECKED_CAST")
+        fun get(): App<F, A> =
+            (ma as Result<*>).getOrThrow() as App<F, A>
     }
 
-    suspend operator fun <B> App<F, B>.component1(): B =
-        this.exec()
+    private val completion = ComprehensionContinuation<F, A>()
 
-    suspend fun <B> App<F, B>.exec(): B =
+    suspend operator fun <B> App<F, B>.component1(): B =
+        this.bind()
+
+    suspend fun <B> App<F, B>.bind(): B =
         currentCoroutineContext().let { coroutineContext ->
             suspendCoroutine { cont ->
                 execute(coroutineContext) {
-                    ma = m.bind<B, B> { b ->
+                    completion.intermediate(Result.success(m.bind<B, B> { b ->
                         cont.resume(b)
                         pure(b)
-                    }(this)
+                    }(this)))
                 }
             }
         }
@@ -36,12 +49,12 @@ class Comprehension<F, A>(private val m: Monad.Core<F>) : Monad.Core<F> by m, Co
         suspend infix fun <F, A> Monad.Core<F>.`do`(f: suspend Comprehension<F, A>.() -> A): App<F, A> =
             invoke(this, f)
 
-        @Suppress("UNCHECKED_CAST")
         suspend operator fun <F, A> invoke(m: Monad.Core<F>, f: suspend Comprehension<F, A>.() -> A): App<F, A> =
             Comprehension<F, A>(m).let { comprehension ->
                 val callback: suspend Comprehension<F, *>.() -> App<F, A> = { returns(comprehension.f()) }
-                callback.startCoroutine(comprehension, comprehension)
-                comprehension.ma as App<F, A>
+                callback.startCoroutine(comprehension, comprehension.completion).let {
+                    comprehension.completion.get()
+                }
             }
     }
 
