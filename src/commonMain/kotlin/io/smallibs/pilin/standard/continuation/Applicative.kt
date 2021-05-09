4271fd951f0c7815e59e4727537f693e617c878a
d-plaindoux <d.plaindoux@free.fr>
2021-05-09 19:09:36 +0200

    Feature: Continuation (preparing user defined effects)

---
+++ src/commonMain/kotlin/io/smallibs/pilin/standard/continuation/Applicative.kt
@@ -0,0 +1,22 @@
+package io.smallibs.pilin.standard.continuation
+
+import io.smallibs.pilin.control.Applicative
+import io.smallibs.pilin.core.Standard.Infix.then
+import io.smallibs.pilin.standard.continuation.Continuation.Companion.continuation
+import io.smallibs.pilin.standard.continuation.Continuation.TK
+import io.smallibs.pilin.standard.continuation.Continuation.TK.Companion.invoke
+import io.smallibs.pilin.type.App
+import io.smallibs.pilin.type.Fun
+
+object Applicative {
+    private class ApplicativeImpl<O> :
+        Applicative.API<TK<O>>,
+        Applicative.WithPureAndApply<TK<O>> {
+        override suspend fun <I> pure(a: I): App<TK<O>, I> = continuation { k -> k(a) }
+        override suspend fun <A, B> apply(mf: App<TK<O>, Fun<A, B>>): Fun<App<TK<O>, A>, App<TK<O>, B>> = { ma ->
+            Continuation { k -> mf { f -> ma(f then k) } }
+        }
+    }
+
+    fun <L> applicative(): Applicative.API<TK<L>> = ApplicativeImpl()
+}
