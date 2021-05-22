4820c585f5ee83afaf014581d36a3fdbdcc92ade
d-plaindoux <d.plaindoux@free.fr>
2021-05-22 09:16:13 +0200

    Basic implementation of Selective applicatice functors

--- src/commonMain/kotlin/io/smallibs/pilin/control/Selective.kt
+++ src/commonMain/kotlin/io/smallibs/pilin/control/Selective.kt
@@ -1,16 +1,37 @@
 package io.smallibs.pilin.control
 
-import io.smallibs.pilin.standard.either.Either
+import io.smallibs.pilin.core.Standard
+import io.smallibs.pilin.core.Standard.curry
+import io.smallibs.pilin.standard.either.Either.Companion.functor
+import io.smallibs.pilin.standard.either.Either.Companion.left
+import io.smallibs.pilin.standard.either.Either.Companion.right
+import io.smallibs.pilin.standard.either.Either.TK
 import io.smallibs.pilin.type.App
 import io.smallibs.pilin.type.Fun
 
 object Selective {
 
-    interface Core<F> {
-        suspend fun <A> pure(a: A): App<F, A>
-        suspend fun <A, B> select(e: Either<A, B>): Fun<App<F, Fun<A, B>>, App<F, B>>
-        suspend fun <A, B, C> branch(e: Either<A, B>): Fun<App<F, Fun<A, C>>, Fun<App<F, Fun<B, C>>, App<F, C>>>
+    interface Core<F> : Applicative.Core<F> {
+        suspend fun <A, B> select(e: App<F, App<TK<A>, B>>): Fun<App<F, Fun<A, B>>, App<F, B>>
+        suspend fun <A, B, C> branch(e: App<F, App<TK<A>, B>>): Fun<App<F, Fun<A, C>>, Fun<App<F, Fun<B, C>>, App<F, C>>>
     }
+
+    interface WithSelect<F> : Core<F> {
+        override suspend fun <A, B, C> branch(e: App<F, App<TK<A>, B>>): Fun<App<F, Fun<A, C>>, Fun<App<F, Fun<B, C>>, App<F, C>>> =
+            curry { l, r ->
+                val a = map<App<TK<A>, B>, App<TK<A>, App<TK<B>, C>>>(functor<A>().map { left(it) })(e)
+                val b = map<Fun<A, C>, Fun<A, App<TK<B>, C>>> { f -> { right(f(it)) } }(l)
+                select(select(a)(b))(r)
+            }
+    }
+
+    interface WithBranch<F> : Core<F> {
+        override suspend fun <A, B> select(e: App<F, App<TK<A>, B>>): Fun<App<F, Fun<A, B>>, App<F, B>> = { r ->
+            branch<A, B, B>(e)(r)(pure(Standard::id))
+        }
+    }
+
+
 
     interface API<F> : Core<F>
 
