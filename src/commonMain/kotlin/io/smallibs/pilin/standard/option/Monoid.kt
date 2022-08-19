a0783f80f6d94d221ba79b959d206e8792b2f426
d-plaindoux <d.plaindoux@free.fr>
2022-08-19 08:11:03 +0200

    Use catamorphism instead of explicit type case

--- src/commonMain/kotlin/io/smallibs/pilin/standard/option/Monoid.kt
+++ src/commonMain/kotlin/io/smallibs/pilin/standard/option/Monoid.kt
@@ -2,19 +2,16 @@ package io.smallibs.pilin.standard.option
 
 import io.smallibs.pilin.abstractions.Monoid
 import io.smallibs.pilin.abstractions.Semigroup
+import io.smallibs.pilin.standard.option.Option.Companion.some
+import io.smallibs.pilin.standard.option.Option.OptionK.Companion.fold
 
 object Monoid {
     class MonoidImp<A>(private val semigroup: Semigroup.API<A>) : Monoid.API<Option<A>> {
         override val neutral: Option<A>
             get() = Option.None
 
-        override suspend fun combine(l: Option<A>, r: Option<A>): Option<A> = when (l) {
-            Option.None -> r
-            is Option.Some -> when (r) {
-                Option.None -> l
-                is Option.Some -> Option.Some(semigroup.combine(l.value, r.value))
-            }
-        }
+        override suspend fun combine(l: Option<A>, r: Option<A>): Option<A> =
+            l.fold({ r }) { lv -> r.fold({ l }) { rv -> some(semigroup.combine(lv, rv)) } }
     }
 
     fun <A> monoid(semigroup: Semigroup.API<A>): Monoid.API<Option<A>> = MonoidImp(semigroup)
