d338ed77e721744bbc9a1b887ed9652ad9afacfd
d-plaindoux <d.plaindoux@free.fr>
2022-08-21 20:25:56 +0200

    Freer in progress

--- src/commonMain/kotlin/io/smallibs/pilin/abstractions/Freer.kt
+++ src/commonMain/kotlin/io/smallibs/pilin/abstractions/Freer.kt
@@ -1,19 +1,20 @@
 package io.smallibs.pilin.abstractions
 
+import io.smallibs.pilin.abstractions.Freer.FreerK
 import io.smallibs.pilin.core.Standard.Infix.then
 import io.smallibs.pilin.type.App
 import io.smallibs.pilin.type.Fun
 
-sealed interface Freer<F, A> : App<Freer.TK<F>, A> {
+sealed interface Freer<F, A> : App<FreerK<F>, A> {
+
+    suspend fun <B> map(f: Fun<A, B>): Freer<F, B> = bind { Return(f(it)) }
+    suspend fun <B> bind(f: Fun<A, Freer<F, B>>): Freer<F, B>
 
     interface Handler<F, A> {
         suspend fun <B> handle(f: Fun<B, A>): Fun<App<F, B>, A>
     }
 
     suspend fun run(f: Handler<F, A>): A
-    suspend fun <B> map(f: Fun<A, B>): Freer<F, B> = bind { Return(f(it)) }
-
-    suspend fun <B> bind(f: Fun<A, Freer<F, B>>): Freer<F, B>
 
     data class Return<F, A>(val value: A) : Freer<F, A> {
         override suspend fun <B> bind(f: Fun<A, Freer<F, B>>) = f(value)
@@ -25,8 +26,8 @@ sealed interface Freer<F, A> : App<Freer.TK<F>, A> {
         override suspend fun run(f: Handler<F, A>): A = f.handle { x: I -> continuation(x).run(f) }(intermediate)
     }
 
-    class TK<F> private constructor() {
-        val <A> App<TK<F>, A>.fix: Freer<F, A> get() = this as Freer<F, A>
+    class FreerK<F> private constructor() {
+        val <A> App<FreerK<F>, A>.fix: Freer<F, A> get() = this as Freer<F, A>
     }
 
     companion object {
